b1d7ba570228741fa5e324453e0bdd5fcf191019
itsaditya07 <64747375+itsaditya07@users.noreply.github.com>
2023-08-18 16:46:06 +0530

    Update passport-local-strategy.js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,50 +4,61 @@ const Employee = require('../models/employee');
 
 // authentication using Passport
 passport.use(new LocalStrategy({
-        usernameField : 'email'
-    },
-    function(email, password, done){
-        Employee.findOne({email : email}, function(err, employee){
-            if(err){
-                console.log("Error in finding the user --> Passport");
-                return done(err);
-            }
-            if(!employee || employee.password != password){
+    usernameField: 'email'
+},
+    async function (email, password, done) {
+
+        try {
+            const employee = await Employee.findOne({ email: email });
+
+
+            if (!employee || employee.password != password) {
                 console.log("Invalid Email/Password while sign in");
                 return done(null, false);
             }
             return done(null, employee);
-        });
+        }
+
+        catch (err) {
+            if (err) {
+                console.log("Error in finding the user --> Passport");
+                return done(err);
+            }
+        }
+
+
     }
 ));
 
 // serializing the user to decide which key is to be kept in cookie
-passport.serializeUser(function(employee, done){
+passport.serializeUser(function (employee, done) {
     done(null, employee._id);
 });
 
 // deserializing the user from the key in the cookie
-passport.deserializeUser(function(id, done){
-    Employee.findById(id, function(err, employee){
-        if(err){
+passport.deserializeUser(async function (id, done) {
+    try {
+        const employee = await Employee.findById(id);
+        return done(null, employee);
+    } catch(err) {
+        if (err) {
             console.log("Error in finding user while deserialized");
             return done(err);
         }
-        return done(null, employee);
-    });
+    }
 });
 
 // made middleware for checking the employee is logged in or not
-passport.checkAuthentication = function(req, res, next){
-    if(req.isAuthenticated()){
+passport.checkAuthentication = function (req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     return res.redirect('/employees/sign_in');
 }
 
 // save the user into the locals
-passport.setAuthenticatedUser = function(req, res, next){
-    if(req.isAuthenticated()){
+passport.setAuthenticatedUser = function (req, res, next) {
+    if (req.isAuthenticated()) {
         res.locals.user = req.user;
     }
     next();
